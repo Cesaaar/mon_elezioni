bac5e6067c1beebafbccbc34d4e9034c802a5b79
Cesaaar <andrea_dmc@hotmail.it>
2017-11-05 19:58:28 +0000

    fb fans

--- elezioni/views/facebook.py
+++ elezioni/views/facebook.py
@@ -4,4 +4,13 @@ from elezioni import app, get_db, render_template, url_for
 # first page
 @app.route('/')
 def facebook():
-    return render_template('facebook.html')
+	engine = get_db()
+	cur = engine.execute(
+	''' 	select fb_fans 
+		from ''' + app.config['SCHEMA_ELE'] + '''."fb_fans"
+		where "user"='Renzi'
+		order by dt_rif desc
+		limit 1;
+	''')
+	fb_fans = cur.fetchall()
+	return render_template('facebook.html',fb_fans=fb_fans)
