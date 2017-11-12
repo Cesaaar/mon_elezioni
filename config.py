4cdf35c1472fdf1d221788055cd637587ffe0466
Andrea Cesarini - OpenData <andrea.cesarini85@gmail.com>
2017-11-12 19:24:36 +0100

    ci 5

---
+++ config.py
@@ -0,0 +1,31 @@
+# coding: utf-8
+
+# Twitter
+TWITTER_KEY = '...'
+TWITTER_SECRET = '...'
+TOKEN = '...'
+TOKEN_SECRET = '...'
+
+# Facebook
+TOKEN_FB = '..'
+FB_APP_ID = '..'
+FB_APP_SECRET = '..'
+FB_APP_USERID = '..'
+
+# Database
+DATABASE_ELE = '..'
+SCHEMA_ELE = '..'
+
+# Applicazione
+HOST = '0.0.0.0'
+PORTA = '5000'
+
+# Utenti Monitorati
+USER1 = 'Renzi'
+USER1_ID_FB = '113335124914'
+
+USER2 = 'Di Maio'
+USER2_ID_FB = '522391027797448'
+
+USER3 = 'Salvini'
+USER3_ID_FB = '252306033154'
