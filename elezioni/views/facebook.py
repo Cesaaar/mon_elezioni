2374816119b4bb3d5205c2a399113451d334cd54
Andrea Cesarini - OpenData <andrea.cesarini85@gmail.com>
2017-11-26 14:14:59 +0100

    FB Page

--- elezioni/views/facebook.py
+++ elezioni/views/facebook.py
@@ -33,36 +33,39 @@ def facebook():
     cur4 = engine.execute(
     '''     select
             "user" as user_post
-            ,to_char(to_date(substring(dt_post from 0 for 11),'YYYY-MM-DD'), 'DD Month YYYY') as dt_post
+            ,substring(dt_post from 12 for 5) as hour_post
+            ,to_char(to_date(substring(dt_post from 0 for 11),'YYYY-MM-DD'), 'DD Month YYYY') as dt_post2
             ,likes as likes_post
             ,msg as msg_post
             from ''' + app.config['SCHEMA_ELE'] + '''."fb_posts"
             where "user"=''' "'" + app.config['USER1'] + "'" '''
-            order by dt_rif desc
+            order by dt_post desc
             limit 1;
     ''')
     
     cur5 = engine.execute(
     '''     select
             "user" as user_post
-            ,to_char(to_date(substring(dt_post from 0 for 11),'YYYY-MM-DD'), 'DD Month YYYY') as dt_post
+            ,substring(dt_post from 12 for 5) as hour_post
+            ,to_char(to_date(substring(dt_post from 0 for 11),'YYYY-MM-DD'), 'DD Month YYYY') as dt_post2
             ,likes as likes_post
             ,msg as msg_post
             from ''' + app.config['SCHEMA_ELE'] + '''."fb_posts"
             where "user"=''' "'" + app.config['USER2'] + "'" '''
-            order by dt_rif desc
+            order by dt_post desc
             limit 1;
     ''')
     
     cur6 = engine.execute(
     '''     select
             "user" as user_post
-            ,to_char(to_date(substring(dt_post from 0 for 11),'YYYY-MM-DD'), 'DD Month YYYY') as dt_post
+            ,substring(dt_post from 12 for 5) as hour_post
+            ,to_char(to_date(substring(dt_post from 0 for 11),'YYYY-MM-DD'), 'DD Month YYYY') as dt_post2
             ,likes as likes_post
             ,msg as msg_post
             from ''' + app.config['SCHEMA_ELE'] + '''."fb_posts"
             where "user"=''' "'" + app.config['USER3'] + "'" '''
-            order by dt_rif desc
+            order by dt_post desc
             limit 1;
     ''')
 
@@ -74,9 +77,13 @@ def facebook():
     fb_post2 = cur5.fetchall()
     fb_post3 = cur6.fetchall()
 
+    id_user = app.config['USER1_ID_FB']
+    id_user2 = app.config['USER2_ID_FB']
+    id_user3 = app.config['USER3_ID_FB']
 
     return render_template('facebook.html',fb_fans=fb_fans,fb_fans2=fb_fans2,fb_fans3=fb_fans3,
-                           fb_post=fb_post, fb_post2=fb_post2, fb_post3=fb_post3)
+                           fb_post=fb_post, fb_post2=fb_post2, fb_post3=fb_post3, id_user=id_user,
+                           id_user2=id_user2, id_user3=id_user3)
 
 # first page
 @app.route('/contatti')
